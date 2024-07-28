f86f8f8f27a2145293cb9d82a994ab2e6f9afb62
AlCkoNi <Tolibjon2004@yandex.ru>
2024-07-28 21:04:47 +0500

    Add more function(db and login)

--- HOTEL/authentication/auth.cs
+++ HOTEL/authentication/auth.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +12,40 @@ namespace HOTEL.authentication
     {
         private string login = "";
         private string passw = "";
+        private int id = 3;
+
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Windows 11\source\repos\HOTEL\HOTEL\Data_Set.mdf"";Integrated Security=True";
+        
         public void purpose(string name, string pass, string role, bool b)//sign-in
         {
+            encrapt(name, pass);
+            string sql = "INSERT INTO users (user_id, login, passwd, role, sysin) VALUES (@user_id, @login, @passwd, @role, @sysin)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@user_id", id);
+                    command.Parameters.AddWithValue("@login", login);
+                    command.Parameters.AddWithValue("@passwd", passw);
+                    command.Parameters.AddWithValue("@role", role);
+                    command.Parameters.AddWithValue("@sysin", b);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        WriteLine("Данные успешно добавлены в базу данных.");
+                    }
+                    else
+                    {
+                        WriteLine("Произошла ошибка при добавлении данных.");
+                    }
+                }
+            }
 
         }
         public void purpose2(string name, string pass,bool b)//log-in
         {
-
+            encrapt(name, pass);
         }
         private void encrapt(string name, string pass)//shifr
         {
